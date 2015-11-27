b11cc13f9c566c9ba116e76e074d09edf56fbc6f
henceee <hg222dv@student.lnu.se>
2015-11-27 19:00:03 +0100

    Why doesn't it commit??
    ??

---
+++ AdventurousContacts/AdventurousContacts/Controllers/ContactController.cs
@@ -0,0 +1,180 @@
+﻿using AdventurousContacts.Models;
+using AdventurousContacts.Models.DataModels;
+using AdventurousContacts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Net;
+
+//TODO ADD BUNDLING N STUFF N THINGS: http://go.microsoft.com/fwlink/?LinkId=254725
+
+namespace AdventurousContacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        #region fields
+        private IRepository _repository;
+        #endregion
+
+        #region controller(s)
+        public ContactController()
+            :this(new Repository())
+        {
+            //empty
+        }
+
+        public ContactController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        #endregion
+        // GET: /Contact/
+         public ActionResult Index()
+        {
+
+            return View("Index", _repository.FindAllContacts());           
+        }
+
+         //
+         // GET: /Contact/Create
+         public ActionResult Create()
+         {
+            return View();
+         }
+
+         //
+         // POST: /Contact/Create
+         [HttpPost]
+         public ActionResult Create([Bind(Include = "FirstName,LastName,EmailAddress")] Contact contact)
+         {
+             if (ModelState.IsValid)
+             {
+                 try
+                 {
+                     _repository.Add(contact);
+                     _repository.Save();
+                     TempData["sucess"] = "Contact saved.";
+                     return RedirectToAction("Index");
+                 }
+                 catch (DataException)
+                 {
+                     TempData["error"] = "Failed to save contact.";
+                 }    
+             }
+             return View();
+         }
+
+        //TODO: Implement ContactController.Delete(int id=0) : ActionResult
+
+        //TODO: Implement ContactController.DeleteContirmed(int? id) : ActionResult
+
+
+         //GET: /Contact(Delete/ID
+         public ActionResult Delete(int? id)
+         {
+              if(!id.HasValue)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }
+
+             var contact = _repository.GetContactById(id.Value);
+
+             if(contact == null)
+             {
+                 return HttpNotFound();
+             }
+
+             return View(contact);
+
+         }
+         //POST: /Contact/Delete/ID
+         [HttpPost]
+         [ActionName("Delete")]
+         [ValidateAntiForgeryToken]
+         public ActionResult DeleteConfirmed(int id=0)
+         {
+             try
+             {
+                 var contactToDelete = new Contact { ContactID = id };
+                 _repository.Delete(contactToDelete);
+                 _repository.Save();
+                 TempData["sucess"] = "Contact deleted.";
+                 
+                 return RedirectToAction("Index");
+             }
+             catch (DataException)
+             {                 
+                  TempData["error"] = "Failed to delete contact.";
+                  
+             }
+
+             return RedirectToAction("Delete", new { id = id });
+         }
+         //Överskugga dispose för att kalla Dispose på Repository obj.
+
+         #region dispose
+         protected override void Dispose(bool disposing)
+         {
+             _repository.Dispose();
+             base.Dispose(disposing);
+
+         }
+         #endregion
+
+         //GET: /Contact/Edit/ID
+         public ActionResult Edit(int? id)
+         {
+             if(!id.HasValue)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }
+
+             var contact = _repository.GetContactById(id.Value);
+
+             if(contact == null)
+             {
+                 return HttpNotFound();
+             }
+
+             return View(contact);
+         }
+        
+         //POST: /Contact/Edit/ID
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+         public ActionResult EditPost(int? id)
+         {
+             var contact = _repository.GetContactById(id.Value);
+
+             if (contact == null)
+             {
+                 return HttpNotFound();
+             }
+
+             if (TryUpdateModel(contact, new string[] { "FirstName", "LastName", "EmailAddress" }))
+             {
+                 try
+                 {
+                     _repository.Update(contact);
+                     _repository.Save();
+                     TempData["sucess"] = "Changes saved.";
+                     return RedirectToAction("Index");
+                 }
+                 catch (DataException)
+                 {
+                      TempData["error"] = "Failed to save changes.";
+                 }
+             }
+
+             return View(contact);
+         }
+  
+
+    }
+}
+
+
---
+++ AdventurousContacts/AdventurousContacts/Repositories/Repository.cs
@@ -0,0 +1,77 @@
+﻿using AdventurousContacts.Models;
+using AdventurousContacts.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AdventurousContacts.Repositories
+{
+    public class Repository : IRepository
+    {
+        
+        private bool _disposed = false;
+
+        private Contact_Entities _entities = new Contact_Entities();
+     
+        public void Add(Contact contact)
+        {
+            _entities.Contact.Add(contact);         
+        }
+
+        public void Delete(Contact contact)
+        {
+            if (_entities.Entry(contact).State == EntityState.Detached)
+            {
+                _entities.Contact.Attach(contact);
+            }
+            _entities.Contact.Remove(contact);
+        }
+
+        public IQueryable FindAllContacts()
+        {
+            return _entities.Contact.AsQueryable();
+        }
+
+        public Models.Contact GetContactById(int contactId)
+        {   
+            return _entities.Contact.Find(contactId);
+        }
+
+        public IList<Contact> GetLastContact(int count = 20)
+        {
+            //TODO: Implement Models.Repository.GetLastContact(int count = 20)
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _entities.SaveChanges();
+        }
+
+        public void Update(Contact contact)
+        {   
+            _entities.Entry(contact).State = EntityState.Modified;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _entities.Dispose();                   
+                }              
+            }
+            _disposed = true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
